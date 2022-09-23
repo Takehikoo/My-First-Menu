dbd6497702b283d2f858b4ddbd6663baa409a832
maria-fernández <mariabelenfernandez1996>
2022-09-23 18:57:52 -0300

    Calculadora changed

---
+++ First Menu Pipe/app.js
@@ -0,0 +1,6 @@
+function MandarSaludos(){
+    let operadorA=document.getElementById("operadorA").innerHTML;
+    let operadorB= document.getElementById("operadorB").innerHTML;
+    let resultado = operadorA+ operadorB;
+    console.log("El resultado de la suma es : " + resultado)
+}
